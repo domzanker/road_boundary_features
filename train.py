41af03be0d93f4ad3f3d676e4c11bb2b2191913c
Dominic Zanker <mail@dzanker.de>
2020-09-29 21:13:48 +0200

    add configs and head definition

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import sys
 import time
 import datetime
 import argparse
+import yaml
 from pathlib import Path
 
 import torch
@@ -15,7 +16,45 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 from utils.dataset import RoadBoundaryDataset
+from utils.losses import CombinedLoss
+from utils.modules import SegmentationHead
 import segmentation_models_pytorch as smp
+
+
+class Params:
+    def __init__(self, config_dict: dict = None, file: Path = None):
+
+        if file is not None:
+            file = Path(file)
+            assert file.suffix == ".yaml"
+            with file.open(mode="rb") as f:
+                config_dict = yaml.safe_load(f)
+
+        for key, val in config_dict.items():
+            if isinstance(val, dict):
+                self.__dict__[key] = Params(config_dict=val)
+            else:
+                self.__dict__[key] = val
+
+    def __str__(self):
+        string, _ = self.__build_str__()
+        return "\n".join(string)
+
+    def __build_str__(self, string: str = [], parents: list = []):
+        for key, val in self.__dict__.items():
+            loc_parents = parents.copy()
+            loc_parents.append(key)
+
+            if isinstance(val, Params):
+                string, loc_parents = val.__build_str__(
+                    string=string, parents=loc_parents
+                )
+            else:
+                s = ".".join(loc_parents)
+                s += ": %s" % val
+                string.append(s)
+        return (string, parents)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,70 +68,77 @@ if __name__ == "__main__":
     parser.add_argument(
         "--cpu_workers", type=int, default=4, help="number of cpu threads for loading"
     )
-    parser.add_argument(
-        "--train_dataset",
-        type=str,
-        default="/home/Zanker/data/nuscenes_train/data/",
-        help="path to training dataset",
-    )
-    parser.add_argument(
-        "--test_dataset",
-        type=str,
-        default="/home/Zanker/data/nuscenes_test/data/",
-        help="path to training dataset",
-    )
-    parser.add_argument(
-        "--evaluation_interval",
-        type=int,
-        default=1,
-        help="number of epochs between evaluations",
-    )
     parser.add_argument("--gpu", type=int, default=0, help="gpu")
     parser.add_argument("--checkpoint_interval", type=int, default=10, help="")
+    parser.add_argument("--configs", type=str, default="config/config.yaml", help="")
 
     opt = parser.parse_args()
     print(opt)
 
+    configs_file = Path(opt.configs)
+    with configs_file.open("rb") as f:
+        configs = yaml.safe_load(f)
+
+    # define device (if available)
     device = torch.device(("cuda:%s" % opt.gpu) if torch.cuda.is_available() else "cpu")
+
     os.makedirs("output", exist_ok=True)
     os.makedirs("training/checkpoints", exist_ok=True)
 
     # Get dataloader
-    dataset = RoadBoundaryDataset(path=Path(opt.train_dataset))
-
-    test_dataset = RoadBoundaryDataset(path=Path(opt.test_dataset))
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=1, shuffle=False, num_workers=1
-    )
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=opt.batch_size,
+    train_dataset = RoadBoundaryDataset(path=Path(configs["dataset"]["train-dataset"]))
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=configs["train"]["batch-size"],
         shuffle=True,
         num_workers=opt.cpu_workers,
         pin_memory=True,
-        # collate_fn=dataset.collate_fn,
     )
-    # build dual loss
-    # distance loss for endpoints and distance map
-    mse_loss = torch.nn.MSELoss(reduction="none")
-    # cosine similarity for direction_map
-    cosine_similarity = torch.nn.CosineSimilarity(dim=1)
+
+    valid_dataset = RoadBoundaryDataset(path=Path(configs["dataset"]["test-dataset"]))
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=1, shuffle=False, num_workers=2
+    )
 
     # Initiate model
-    model = smp.Linknet(in_channels=4, encoder_depth=3, classes=4, activation="sigmoid")
+    model_configs = configs["model"]
+    if not model_configs["use_custom_encoder"]:
+        model = smp.Linknet(
+            encoder_name=model_configs["encoder"],
+            encoder_weights=model_configs["encoder_weights"],
+            in_channels=4,
+            encoder_depth=model_configs["encoder_depth"],
+            classes=4,
+            activation=model_configs["activation"],
+            decoder_use_batchnorm=model_configs["decoder_use_batchnorm"],
+        )
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters())
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(
+        encoder_name=model_configs["encoder"],
+        pretrained=model_configs["encoder_weights"],
+    )
+
+    segmentation_head = SegmentationHead(branch_definition=model_configs["head"])
+
+    loss = CombinedLoss()
+    # cosine similarity for direction_map
+    # TODO learning rate
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=configs["train"]["learning_rate"],
+        weight_decay=configs["train"]["weight_decay"],
+    )
 
     tb_writer = SummaryWriter("tensorboard/road_boundary")
 
-    num_epochs = 100
+    num_epochs = configs["train"]["epochs"]
     for epoch in range(num_epochs):
         model.train()
         start_time = time.time()
-        for batch_i, (imgs, targets) in enumerate(dataloader):
+        for batch_i, (imgs, targets) in enumerate(train_loader):
 
-            batches_done = len(dataloader) * epoch + batch_i
+            batches_done = len(train_loader) * epoch + batch_i
 
             imgs = Variable(imgs.to(device))
             targets = Variable(targets.to(device), requires_grad=False)
@@ -105,32 +151,22 @@ if __name__ == "__main__":
             # tb_writer.add_images("label_direction", label_direction, batches_done)
             tb_writer.add_images("label_end", label_end, batches_done)
 
-            outputs = model(imgs)
-            feature_distance = outputs[:, 0:1, :, :]
-            feature_end = outputs[:, 1:2, :, :]
-            feature_direction = outputs[:, 2:4, :, :]
+            (feature_distance, feature_end, feature_direction) = model(imgs)
 
             # tb_writer.add_images("feature_direction", feature_direction, batches_done)
             tb_writer.add_images("feature_distance", feature_distance, batches_done)
             tb_writer.add_images("feature_end", feature_end, batches_done)
 
             # L(S,E,D) = l_det(S) + k1 * l_end(E) + k2 * l_dir(D)
-            k1 = 10
-            k2 = 10
-            distance_loss = mse_loss(feature_distance, label_distance)
-            end_loss = mse_loss(feature_end, label_end)
-            direction_loss_0 = cosine_similarity(feature_direction, label_direction)
-
-            loss = (
-                direction_loss_0
-                # + direction_loss_1[:, :, :]
-                + k1 * distance_loss
-                + k2 * end_loss
-            ).sum()
+            lo = loss(
+                distance=(feature_distance, label_distance),
+                end_points=(feature_end, label_end),
+                direction=(feature_direction, label_direction),
+            )
 
             tb_writer.add_scalar("loss", loss, batches_done)
 
-            loss.backward()
+            lo.backward()
 
             optimizer.step()
             optimizer.zero_grad()
@@ -139,30 +175,30 @@ if __name__ == "__main__":
             #   Log progress
             # ----------------
 
-            log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (
+            log_str = "\n>>>> [Epoch %d/%d, Batch %d/%d] <<<< \n" % (
                 epoch,
                 num_epochs,
                 batch_i,
-                len(dataloader),
+                len(train_loader),
             )
 
             # Determine approximate time left for epoch
-            epoch_batches_left = len(dataloader) - (batch_i + 1)
+            epoch_batches_left = len(train_loader) - (batch_i + 1)
             time_left = datetime.timedelta(
                 seconds=epoch_batches_left * (time.time() - start_time) / (batch_i + 1)
             )
-            log_str += f"\n---- ETA {time_left}"
+            log_str += f"\n>>>> ETA {time_left}"
 
             print(log_str)
 
             tb_writer.flush()
 
-        if epoch % opt.evaluation_interval == 0:
+        if epoch % configs["train"]["eval-interval"] == 0:
             model.eval()
-            print("\n---- Evaluating Model ----")
+            print("\n>>>> Evaluating Model <<<<")
             with torch.no_grad():
                 # FIXME
-                for imgs, targets in test_loader:
+                for imgs, targets in valid_loader:
                     imgs = Variable(imgs.to(device))
                     targets = Variable(targets.to(device), requires_grad=False)
 
@@ -170,33 +206,20 @@ if __name__ == "__main__":
                     label_end = targets[:, 1, :, :]
                     label_direction = targets[:, 2:4, :, :]
 
-                    outputs = model(imgs)
-                    feature_distance = outputs[:, 0, :, :]
-                    feature_end = outputs[:, 1, :, :]
-                    feature_direction = outputs[:, 2:4, :, :]
+                    (feature_distance, feature_end, feature_direction) = model(imgs)
 
                     # L(S,E,D) = l_det(S) + k1 * l_end(E) + k2 * l_dir(D)
-                    k1 = 10
-                    k2 = 10
-                    distance_loss = mse_loss(feature_distance, label_distance)
-                    end_loss = mse_loss(feature_end, label_end)
-                    direction_loss_0 = cosine_similarity(
-                        feature_direction, label_direction
+                    lo = loss(
+                        distance=(feature_distance, label_distance),
+                        end_points=(feature_end, label_end),
+                        direction=(feature_direction, label_direction),
                     )
-
-                    loss = (
-                        direction_loss_0
-                        # + direction_loss_1[:, :, :]
-                        + k1 * distance_loss
-                        + k2 * end_loss
-                    ).sum()
                     print("Loss on test set: %s" % loss)
-                    break
 
             # Evaluate the model on the validation set
             # TODO
 
-        if epoch % opt.checkpoint_interval == 0:
+        if epoch % configs["train"]["checkpoint-interval"] == 0:
             torch.save(
                 model.state_dict(), "training/checkpoints/linknet_%d.pth" % epoch
             )
