fix scaling

utils/feature_net.py
@@ -53,13 +53,14 @@ class Decoder(Module):
 
     def forward(self, *features):
         # features = features[1:]
+        # [print(f.shape) for f in features]
         features = features[::-1]
 
         x = features[0]
         skips = features[1:]
 
         for i, block in enumerate(self.blocks):
-            skip = features[i] if i < len(skips) else None
+            skip = skips[i] if i < len(skips) else None
             x = block(x, skip)
         return x
 
@@ -75,7 +76,7 @@ class DecoderBlock(Module):
         dilation: Union[_size_2_t, List[_size_2_t]] = 0,
         activation: str = "relu",
         upsampling_mode: str = "nearest",
-        upsampling_factor: int = 2,
+        upsampling: Optional[_size_2_t] = 2,
         apply_instance_norm: bool = False,
     ):
         super(DecoderBlock, self).__init__()
@@ -107,8 +108,14 @@ class DecoderBlock(Module):
             ]
         )
         self.activate = activation_func(activation)
+        if isinstance(upsampling, list):
+            size = upsampling
+            factor = None
+        else:
+            size = None
+            factor = upsampling
         self.upsample = torch.nn.Upsample(
-            scale_factor=upsampling_factor, mode=upsampling_mode
+            size=size, scale_factor=factor, mode=upsampling_mode
         )
         if apply_instance_norm:
             self.instance_normalize = torch.nn.ModuleList(

utils/losses.py
@@ -1,24 +1,41 @@
 import torch
 
+from typing import Optional, Callable, Union
+
+regression_losses = torch.nn.ModuleDict(
+    {
+        "mse_mean": torch.nn.MSELoss(reduction="mean"),
+        "mse_sum": torch.nn.MSELoss(reduction="sum"),
+        "cosine": torch.nn.CosineSimilarity(),
+        "bce": torch.nn.BCELoss(),
+    }
+)
+
 
 class CombinedLoss:
     def __init__(
         self,
-        regression_loss=torch.nn.MSELoss(reduction="none"),
-        distance_loss=torch.nn.CosineSimilarity(dim=1),
+        regression_loss: Union[str, Callable] = torch.nn.MSELoss(reduction="none"),
+        distance_loss: Union[str, Callable] = torch.nn.CosineSimilarity(dim=1),
         *,
-        k1=10,
-        k2=10
+        k1: float = 10,
+        k2: float = 10
     ):
         self.regression_loss = regression_loss
         self.distance_loss = distance_loss
         self.k1 = k1
         self.k2 = k2
 
-    def __call__(self, distance, end_points, direction):
-        loss = (
-            self.distance_loss(direction[0], direction[1])
-            + self.k1 * self.regression_loss(distance[0], distance[1])
-            + self.k2 * self.regression_loss(end_points[0], end_points[1])
+    def __call__(self, predictions, targets):
+        distance, end_points, direction = predictions
+        distance_t, end_points_t, direction_t = targets
+
+        l_DirMap = self.distance_loss(direction, direction_t)
+        l_DistMap = self.regression_loss(distance, distance_t)
+        l_EndMap = self.regression_loss(end_points, end_points_t)
+        loss = l_DirMap + self.k1 * l_DistMap + self.k2 * l_EndMap
+
+        return (
+            loss.sum(),
+            {"l_DirMap": l_DirMap, "l_DistMap": l_DistMap, "l_EndMap": l_EndMap},
         )
-        return loss.sum()

utils/modules.py
@@ -48,7 +48,7 @@ class SegmentationHead(torch.nn.Module):
         self,
         branches: Union[List[List[int]], List[Dict[str, Any]]],
         upsample_mode: str = "nearest",
-        scale_factor: float = 2.0,
+        scale_factor: float = 4.0,
         **kwargs
     ):
 
@@ -173,6 +173,7 @@ class SegmentationBranch(torch.nn.Module):
         )
 
     def forward(self, x: torch.Tensor):
+        x = self.upsampling(x)
         for block in self.conv_blocks:
             x = block(x)
         return x