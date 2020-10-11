6edca775fab3c74977b0bf55bfc950a51b9a9c86
Dominic Zanker <mail@dzanker.de>
2020-10-11 10:50:14 +0200

    torch

--- utils/dataset.py
+++ utils/dataset.py
@@ -121,6 +121,7 @@ class RoadBoundaryDataset(Dataset):
         if self.transform:
             rgb = self.transform(rgb)
 
+        print(height.shape)
         image_torch = torch.cat([rgb, height])
         image_torch = F.interpolate(
             image_torch[None, :, :, :], size=self.image_size
