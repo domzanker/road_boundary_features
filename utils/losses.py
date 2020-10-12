403859f8df5005e6e133b26e03e4c92aa586abbd
Dominic Zanker <mail@dzanker.de>
2020-10-12 18:22:14 +0200

    add nll loss

--- utils/losses.py
+++ utils/losses.py
@@ -1,5 +1,5 @@
 from torch.nn import ModuleDict
-from torch.nn import MSELoss, CrossEntropyLoss, BCELoss
+from torch.nn import MSELoss, CrossEntropyLoss, BCELoss, NLLLoss
 from typing import Optional, Callable, Union
 
 
@@ -9,5 +9,6 @@ def loss_func(loss: str, reduction: str = "mean", **kwargs):
             "mse": MSELoss(reduction=reduction),
             "cross_entropy": CrossEntropyLoss(reduction=reduction, **kwargs),
             "bce": BCELoss(reduction=reduction, **kwargs),
+            "nll": NLLLoss(reduction=reduction, **kwargs),
         }
     )[loss]
