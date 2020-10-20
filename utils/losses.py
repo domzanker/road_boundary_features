7ddfb854aed46f4275368a18cef595d86fcfeebc
Dominic Zanker <mail@dzanker.de>
2020-10-20 07:56:24 +0200

    add MultiObjectiveLoss declaration

--- utils/losses.py
+++ utils/losses.py
@@ -1,6 +1,6 @@
-from torch.nn import ModuleDict
+from torch.nn import ModuleDict, ModuleList, Module
 from torch.nn import MSELoss, CrossEntropyLoss, BCELoss, NLLLoss
-from typing import Optional, Callable, Union
+from typing import Optional, Callable, Union, List
 
 
 def loss_func(loss: str, reduction: str = "mean", **kwargs):
@@ -12,3 +12,14 @@ def loss_func(loss: str, reduction: str = "mean", **kwargs):
             "nll": NLLLoss(reduction=reduction, **kwargs),
         }
     )[loss]
+
+
+class MultiObjectiveLoss(Module):
+    def __init__(self, losses: List[str], factors: List[float]):
+        super(MultiObjectiveLoss, self).__init()
+
+        self.factors = factors
+        self.losses = ModuleList([loss_func(f) for f in losses])
+
+    def forward(self, x):
+        raise NotImplementedError
